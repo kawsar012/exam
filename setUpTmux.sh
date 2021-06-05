c33d7d64ed484308fe5a35f935c3765003133bff
stranger2unix <butex.kawsar@gmail.com>
2021-06-05 18:24:26 +0600

    update setupscript

--- setUpTmux.sh
+++ setUpTmux.sh
@@ -5,5 +5,4 @@ pkg update
 pkg install git wget -y
 cd
 git clone https://github.com/stranger2unix/exam /storage/emulated/0/exam
-wget https://gist.githubusercontent.com/marioBonales/1637696/raw/93a33aa5f1893f46773483250214f8b8b496a270/.bashrc
-echo "cd /storage/emulated/0/" >> .bashrc
+cp /storage/emulated/0/exam/.bashrc ~/
