638b99c2bcfc36fb05259e3e4bef7aaf8162c1f5
stranger2unix <butex.kawsar@gmail.com>
2021-06-05 14:15:51 +0600

    update setupscript

--- setUpTmux.sh
+++ setUpTmux.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+termux-setup-storage
 pkg update
 pkg install git wget -y
 cd
 git clone https://github.com/stranger2unix/exam /storage/emulated/0/exam
-termux-setup-storage
 wget https://gist.githubusercontent.com/marioBonales/1637696/raw/93a33aa5f1893f46773483250214f8b8b496a270/.bashrc
 echo "cd /storage/emulated/0/" >> .bashrc
